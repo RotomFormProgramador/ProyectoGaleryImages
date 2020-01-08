19489fa4526f2c289b0de73c6d9afd479a0f7d60
RotomFormProgramador <juniorbenjamin12@hotmail.com>
2020-01-08 06:02:38 +0000

    css practic

--- frontend/src/component/Navbar/Navbar.js
+++ frontend/src/component/Navbar/Navbar.js
@@ -2,21 +2,24 @@ import React from "react"
 import {Link} from 'react-router-dom'
 import "../Navbar/navbar.css"
 
+
+//<img className="Logo" src="https://toppng.com/uploads/preview/camera-logo-png-hd-11563110785oo04j22ddv.png"></img>
+
 class Navbar extends React.Component{
 
 
     render(){
-        return <nav>
-                        <ul>
-                            <li ><Link to={"/"} >Home</Link></li>
+        return <div className="navbar">
+                        <ul className="nveg">
+                            <li ><Link  to={"/"} >Home</Link></li>
+                        
+                            <li ><Link  to={"/About"} >About</Link></li>
+                        
+                            <li ><Link  to={"/suport"} >Suport</Link></li>
+
+
                         </ul>
-                        <ul>
-                            <li ><Link to={"/About"} >About</Link></li>
-                        </ul>
-                        <ul>
-                            <li ><Link to={"/suport"} >Suport</Link></li>
-                        </ul>
-                </nav>
+                </div>
     }
 }
 
