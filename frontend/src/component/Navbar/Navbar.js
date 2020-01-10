a9bb862a434901e9eab9b341d6cc0ba6268f0722
RotomFormProgramador <juniorbenjamin12@hotmail.com>
2020-01-10 05:20:00 +0000

    Logo

--- frontend/src/component/Navbar/Navbar.js
+++ frontend/src/component/Navbar/Navbar.js
@@ -3,25 +3,25 @@ import {Link} from 'react-router-dom'
 import "../Navbar/navbar.css"
 
 
+
+
 //<img className="Logo" src="https://toppng.com/uploads/preview/camera-logo-png-hd-11563110785oo04j22ddv.png"></img>
 
 class Navbar extends React.Component{
 
 
     render(){
-        return <div className="navbar">
-                        <ul className="nveg">
-                            <li ><Link  to={"/"} >Home</Link></li>
-                        
-                            <li ><Link  to={"/About"} >About</Link></li>
-                        
-                            <li ><Link  to={"/suport"} >Suport</Link></li>
+        return<nav className="navIni"> 
 
-
-                        </ul>
-                </div>
+            <Link  to={"/"} >Home</Link>
+            <Link  to={"/About"} >About</Link>
+            <Link  to={"/Suport"} >Suport</Link>
+            
+            <img className="Logo" src="https://github.com/RotomFormProgramador/ProyectoGaleryImages/blob/19489fa4526f2c289b0de73c6d9afd479a0f7d60/frontend/public/Logo.png" />
+        </nav>
     }
 }
+
 
 
 export default Navbar;
